6498b60158fe957545719d9a9501d511b5619f7f
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-04-29 14:44:36 -0300

    Initial project
    Initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datomic-ecommerce
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/datomic_ecommerce/core_test.clj
@@ -0,0 +1,7 @@
+(ns datomic-ecommerce.core-test
+  (:require [clojure.test :refer :all]
+            [datomic-ecommerce.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
